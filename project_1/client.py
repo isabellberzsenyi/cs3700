87ff6b3d440270b0c096fe5ce7e92e696aec5409
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-01-24 14:58:14 -0500

    trying to connect

--- project_1/client.py
+++ project_1/client.py
@@ -1,1 +1,14 @@
-print("HELLO")
+import socket, sys
+
+HOST = 'simple-service.css.neu.edu'
+PORT = 27995
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('1')
+try:
+  s.connect((HOST, PORT))
+except:
+  print('BAD')
+  sys.exit()
+
+print('Connected')
