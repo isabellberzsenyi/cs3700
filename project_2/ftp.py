99d41192e6423f0ca6dad82d9378379bfd9199b6
Isabell Berzsenyi <iberzsenyi@login-students.ccs.neu.edu>
2021-01-29 17:38:09 -0500

    connect to ftp

--- project_2/ftp.py
+++ project_2/ftp.py
@@ -2,7 +2,7 @@ import socket, sys, re
 OPERATION = sys.argv[1]
 ARG1 = sys.argv[2]
 ARG1 = ARG1.split('//')[1]
-parse = re.split('[-:@]', ARG1)
+parse = re.split('[:@]', ARG1)
 
 USER = parse[0]
 if USER == '':
@@ -17,10 +17,11 @@ PATH = split_hostname[0]
 
 if (len(sys.argv) == 4):
   ARG2 = sys.argv[3]
-  # print(ARG2)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
   s.connect((HOST, 21))
+  print("CONNECTED")
 except:
+  print('UH OH')
   sys.exit()
