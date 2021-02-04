a328cca29ff4d1c6234e295362224c36deb22c70
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-02-04 10:42:14 -0500

    removing repetition

--- project_2/ftp.py
+++ project_2/ftp.py
@@ -1,5 +1,6 @@
 import socket, sys, re, os
 OPERATION = sys.argv[1]
+
 if len(sys.argv) == 4:
   if sys.argv[2].find("ftp") == -1:
     ARG1 = sys.argv[3]
@@ -15,19 +16,16 @@ else:
 ARG1 = ARG1.split('//')[1]
 parse_args = ARG1.split('@')
 
-
 USER = parse_args[0].split(':')[0]
 if len(parse_args[0].split(':')) == 2:
   PASSWORD = parse_args[0].split(':')[1]
 split_host = parse_args[1].split(':')
+
 if len(split_host) == 1:
   HOST = split_host[0].split('/', 1)[0]
-
   if len(split_host[0].split('/', 1)) == 2:
     PATH = split_host[0].split('/', 1)[1]
-    print('path', PATH)
 elif len(split_host) == 2:
-  # given port
   HOST = split_host[0]
   PORT = split_host[1].split('/', 1)[0]
   PATH = split_host[1].split('/', 1)[1]
@@ -35,57 +33,46 @@ elif len(split_host) == 2:
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
   s.connect((HOST, 21))
-  print("CONNECTED")
 except:
-  print('UH OH')
+  print('Error connecting to host socket')
   sys.exit()
 
 s.send('USER {}\r\n'.format(USER))
-
 while 1:
-  print("WHILE LOOP") 
   response = s.recv(8192)
-  print("1", response)
+  print(response)
   if response.find('Please specify') != -1:
-    print(response.find('Please specify'))
-    break;
+    break
 if PASSWORD:
   s.send('PASS ' + PASSWORD + '\r\n')
   response = s.recv(8192)
-  print("2", response)
+  print(response)
 
-print('set type')
 s.send('TYPE I\r\n')
 response = s.recv(8192)
 print(response)
 
-print('set mode')
 s.send('MODE S\r\n')
 response = s.recv(8192)
 print(response)
 
-print('set stru')
 s.send('STRU F\r\n')
 response = s.recv(8192)
 print(response)
 
 if OPERATION == 'mkdir':
-  print('mkdir')
   s.send('MKD ' + PATH + '\r\n')
   response = s.recv(8192)
   print(response)
 elif OPERATION == 'rmdir':
-  print('rmdir')
   s.send('RMD ' + PATH + '\r\n')
   response = s.recv(8192)
   print(response)
 elif OPERATION == 'rm':
-  print('remove')
   s.send('DELE ' + PATH + '\r\n')
   response = s.recv(8192)
   print(response)
 else:
-  print('opening a channel')
   s.send('PASV\r\n')
   response = s.recv(8192)
   print(response)
@@ -93,16 +80,14 @@ else:
   IP_PORT = re.split('[()]', response)[1]
   IP = reduce(lambda a, b: a + '.' + b,IP_PORT.split(',')[0:4])
   PORT2 = (int(IP_PORT.split(',')[4:][0]) << 8) + int(IP_PORT.split(',')[4:][1])
-  print('opening data channel')
+  
   data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   try:
     data.connect((IP, PORT2))
-    print("data channel connected")
   except:
     print('Error connecting to data channel')
     sys.exit()
   if OPERATION == 'ls':
-    print('list')
     s.send('LIST ' + PATH + '\r\n')
     response = s.recv(8192)
     print(response)
@@ -110,25 +95,25 @@ else:
     print(response)
     response = s.recv(8192)
     print(response)
-  elif OPERATION == 'cp':
+  elif OPERATION == 'cp' or OPERATION == 'mv':
     if DIRECTION == 'send':
-      print('store')
       s.send('STOR ' + PATH + '\r\n')
-      response = s.recv(8192).split(" ")[0]
+      response = s.recv(8192)
       print(response)
-      if response == "150":
+      if response.split(" ")[0] == "150":
         f = open(ARG2, "r")
         data.send(f.read())
         data.close()
         f.close()
         response = s.recv(8192)
         print(response)
+        if OPERATION == 'mv':
+          os.remove(ARG2)
     else:
-      print('receive')
       s.send('RETR ' + PATH + '\r\n')
-      response = s.recv(8192).split(" ")[0]
+      response = s.recv(8192)
       print(response)
-      if response == "150":
+      if response.split(" ")[0] == "150":
         f = open(ARG2, "w")
         response = data.recv(8192)
         print(response)
@@ -136,13 +121,17 @@ else:
         f.close()
         response = s.recv(8192)
         print(response)
+        if OPERATION == 'mv':
+          s.send('DELE ' + PATH + '\r\n')
+          response = s.recv(8192)
+          print(response)
+  """
   elif OPERATION == 'mv':
     if DIRECTION == 'send':
-      print('store')
       s.send('STOR ' + PATH + '\r\n')
-      response = s.recv(8192).split(" ")[0]
+      response = s.recv(8192)
       print(response)
-      if response == "150":
+      if response.split(" ")[0] == "150":
         f = open(ARG2, "r")
         data.send(f.read())
         data.close()
@@ -150,8 +139,8 @@ else:
         response = s.recv(8192)
         print(response)
         os.remove(ARG2)
+    
     else:
-      print('receive')
       s.send('RETR ' + PATH + '\r\n')
       response = s.recv(8192)
       print(response)
@@ -166,8 +155,8 @@ else:
         s.send('DELE ' + PATH + '\r\n')
         response = s.recv(8192)
         print(response)
+        """
 
-print('quit')
 s.send('QUIT\r\n')
 response = s.recv(8192)
 print(response)
