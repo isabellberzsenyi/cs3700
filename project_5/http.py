3ebc4da501dd28d9e394dc70db993fb698b1a565
Ameya Bhat <ameya@Ameyas-MacBook-Air.local>
2021-03-28 20:41:17 -0400

    Establish initial socket connection, and partially implement HTTP GET
    We're getting responses back from GET, but we still need to decide on how
    exactly we want to parse and return our data. Initial thoughts are pulling
    out the body, cookies, and checking response (probably need to return a response code?)

--- project_5/http.py
+++ project_5/http.py
@@ -1,19 +1,26 @@
 import sys
 import socket
-
+from urllib.parse import urlparse
 
 def POST():
   return 1
 
-def GET(HOST, data):
+def GET(url):
+  parsed_url = urlparse(url)
+
+  # Should probably parse this into some useable object?? 
+  # Or leave it as text idk - I'd want a library to pull the relevant info for me
+
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.settimeout(30)
   
   try:
-    s.connect((HOST, 80))
+    s.connect((parsed_url.hostname, 80))
   except (ValueError, KeyError, TypeError) as e:
     print(e)
     sys.exit()
+
+  data = f"GET {parsed_url.path} HTTP/1.0\r\nHOST: {parsed_url.hostname}\n\n"
   
   s.send(data.encode('utf-8'))
 
@@ -24,6 +31,10 @@ def GET(HOST, data):
       if '</html>' in response:
         break
     print(response)
+
+  # check the response
+  # pull of cookies
+  # probably parse things into header, body, etc?
 
   except socket.error as e:
     print(e)
